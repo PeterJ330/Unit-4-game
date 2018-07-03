426898328a007dbd9a2bf084881b77b604ebe326
Peter Jarrard <peter.a.jarrard@gmail.com>
2018-07-02 20:11:08 -0500

    character and enemy var working, next step is defender var

---
+++ assets/javascript/game.js
@@ -0,0 +1,166 @@
+
+
+
+var skywalker = {
+    health: 100 ,
+    attack: 8 , 
+    newAttack: 0,
+    counter: 5 ,
+    character: false,
+    enemy: false,
+    defender: false,
+} ;
+
+var fett = {
+    health: 80 ,
+    attack: 10 , 
+    newAttack: 0,
+    counter: 7 ,
+    character: false,
+    enemy: false,
+    defender: false,
+} ;
+
+var vader = {
+    health: 70 ,
+    attack: 12, 
+    newAttack: 0,
+    counter: 9 ,
+    character: false,
+    enemy: false,
+    defender: false,
+} ;
+
+var solo = {
+    health: 90 ,
+    attack: 16 , 
+    newAttack: 0,
+    counter: 12 ,
+    character: false,
+    enemy: false,
+    defender: false,
+} ;
+
+
+var userCharacter = "" ;
+// var enemy = "" ;
+// var defender = "" ;
+
+
+// function that increases user character attack points by base value
+
+
+$(document).ready(function() {
+
+
+// console.log(skywalker, fett, vader, solo);
+console.log("Initial User Character: "+ userCharacter);
+//=============================================================================
+//
+$(".button").on("click", function characterSelect() {
+    userCharacter = $(this).attr("value");
+    console.log("User Character: "+ userCharacter);
+
+   // function enemy(){
+        if (userCharacter === "skywalker"){
+            skywalker.character = true;
+            skywalker.enemy = false;
+            fett.character = false;
+            fett.enemy = true;
+            vader.character = false;
+            vader.enemy = true;
+            solo.character = false;
+            solo.enemy = true; 
+        } else if (userCharacter === "fett"){
+            skywalker.character = false;
+            skywalker.enemy = true;
+            fett.character = true;
+            fett.enemy = false;
+            vader.character = false;
+            vader.enemy = true;
+            solo.character = false;
+            solo.enemy = true;                         
+        } else if (userCharacter === "vader"){
+            skywalker.character = false;
+            skywalker.enemy = true;
+            fett.character = false;
+            fett.enemy = true;
+            vader.character = true;
+            vader.enemy = false;
+            solo.character = false;
+            solo.enemy = true;  
+        } else if (userCharacter === "solo"){
+            skywalker.character = false;
+            skywalker.enemy = true;
+            fett.character = false;
+            fett.enemy = true;
+            vader.character = false;
+            vader.enemy = true;
+            solo.character = true;
+            solo.enemy = false; 
+        }
+    //}
+    //console.log(skywalker, fett, vader, solo);
+    console.log("Character");
+    console.log(skywalker.character, fett.character, vader.character, solo.character);
+    console.log("Enemy");
+    console.log(skywalker.enemy, fett.enemy, vader.enemy, solo.enemy);
+})
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//=============================================================================
+//
+$(".button").on("click", function defender() {
+    if (skywalker.character === true || fett.character === true || vader.character === true || solo.character === true){
+        console.log("nothing should happen");
+    } else if (skywalker.character === false && skywalker.enemy === true){
+        skywalker.defender === true;
+    } else if (fett.character === false && fett.enemy === true){
+        fett.defender === true;
+    } else if (vader.character === false && vader.enemy === true){
+        vader.defender === true;
+    } else if (solo.character === false && solo.enemy === true){
+        solo.defender === true;
+    }
+    console.log(skywalker.defender, fett.defender, vader.defender, solo.defender);
+})
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//=============================================================================
+//
+$(".attackBtn").on("click",function attack(){
+    if (userCharacter === ""){
+        console.log("No User Character Selected");
+    } else if (userCharacter === "skywalker") {
+        skywalker.newAttack += skywalker.attack;
+    } else if (userCharacter === "fett") {
+        fett.newAttack += fett.attack;
+    } else if (userCharacter === "vader") {
+        vader.newAttack += vader.attack;
+    } else if (userCharacter === "solo") {
+        solo.newAttack += solo.attack;
+    }
+    console.log("attack user character: "+ userCharacter);
+    // console.log(skywalker, fett, vader, solo);
+    
+})
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+//function reset()
+
+
+
+
+
+})
